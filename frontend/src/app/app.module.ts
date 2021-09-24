5356d21cb5e9a9882999e13792e80fcc27fc0f25
M-A-Saldanha <72634705+M-A-Saldanha@users.noreply.github.com>
2021-09-23 21:08:02 -0300

    Adicionado frontend do crud do produto

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,76 @@
+import { LOCALE_ID, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/template/header/header.component';
+import { HttpClientModule } from '@angular/common/http'
+import localePt from '@angular/common/locales/pt';
+import {registerLocaleData} from '@angular/common';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav'
+import {MatCardModule} from '@angular/material/card'
+import {MatButtonModule} from '@angular/material/button'
+import {MatListModule} from '@angular/material/list';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import{ FormsModule } from'@angular/forms';
+import{ MatFormFieldModule} from'@angular/material/form-field';
+import{MatInputModule} from'@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+
+import { FooterComponent } from './components/template/footer/footer.component';
+import { NavComponent } from './components/template/nav/nav.component';
+import { HomeComponent } from './components/views/home/home.component';
+import { ProductCrudComponent } from './components/views/product-crud/product-crud.component';
+import { ProductCreateComponent } from './components/product/product-create/product-create.component';
+import { ProductReadComponent } from './components/product/product-read/product-read.component'
+import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component'
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
+import { LoginComponent } from './components/views/login/login.component'
+
+
+registerLocaleData(localePt)
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    HomeComponent,
+    ProductCrudComponent,
+    ProductCreateComponent,
+    ProductReadComponent,
+    ProductDeleteComponent,
+    ProductUpdateComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatCardModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    HttpClientModule
+  ],
+  providers: [
+   { provide: LOCALE_ID,
+    useValue: 'pt-BR'}
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
